abda58e3f03784e21e8069f7b261f2dbe1e53cf8
domoticon21-netizen <domoticon21@gmail.com>
2026-02-06 19:17:35 +0000

    vallition

--- spotify/backoffice/src/app/api/bands/route.ts
+++ spotify/backoffice/src/app/api/bands/route.ts
@@ -1,6 +1,16 @@
 import { NextResponse } from "next/server";
 import { prisma } from '../../../../lib/prisma'
-import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
+import * as z from "zod";
+//import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
+//import path from "path";
+//import { mkdir, writeFile } from "fs/promises";
+
+ const bandSchemma = z.object({ 
+   name: z.string().min(1),
+   slug: z.string().min(1),
+   description: z.string().optional(),
+   status: z.enum(["active", "inactive"])
+  });
 
 // GET
 export async function GET() {
@@ -8,13 +18,57 @@ export async function GET() {
   return NextResponse.json(dados);
 }
 
-// POST
 export async function POST(request: Request) {
-  const body = await request.json();
-  const params = new URLSearchParams(body);
-  console.log(params)
+  const bodyText = await request.text();
+  const params = new URLSearchParams(bodyText)
+  const name = params.get("name");
+  const slug = params.get("slug");
+  const description = params.get("description");
+  const status = params.get("status"); 
+
+  const validate = bandSchemma.parse({
+    
+  });
+
+  return NextResponse.json(
+    {
+      name: name,
+      slug: slug,
+      description: description,
+      status: status
+    })
 }
 
+
+// POST formdata
+/*
+export async function POST(request: Request) {
+  const data = await request.formData();  
+
+  const file = data.get("cover");
+
+   if (!(file instanceof File)) {
+     return NextResponse.json(
+       { erro: "não é uma imagem" },
+       { status: 400 }
+     );
+   }
+  
+   const arbufer = await file.arrayBuffer()
+   const bufer = Buffer.from(arbufer);
+
+   const upload_dir = path.join(process.cwd(), "public", "upload");
+   await mkdir(upload_dir, { recursive:true });
+
+   const filePath = path.join(upload_dir, file.name);
+   await writeFile(filePath, bufer);
+
+
+  return NextResponse.json({
+    received: `upload/${file.name}` ,
+  });
+}
+*/
 // PUT
 export async function PUT(request: Request) {
   const body = await request.json();
